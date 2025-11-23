d7a60ddabe1bd02d758e6a797b62f38548ec80ad
Thomas Eizinger <thomas@eizinger.io>
2025-11-23 16:46:46 +1100

    feat: implement `PartialOrd` & `Ord` for alignments

--- src/alignment.rs
+++ src/alignment.rs
@@ -1,24 +1,24 @@
 /// Zero-sized type (ZST) to enforce 8-byte memory alignment
 #[repr(align(8))]
-#[derive(PartialEq, Eq, Copy, Clone, Debug)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Debug)]
 pub struct Align8;
 
 /// Zero-sized type (ZST) to enforce 16-byte memory alignment
 #[repr(align(16))]
-#[derive(PartialEq, Eq, Copy, Clone, Debug)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Debug)]
 pub struct Align16;
 
 /// Zero-sized type (ZST) to enforce 32-byte memory alignment
 #[repr(align(32))]
-#[derive(PartialEq, Eq, Copy, Clone, Debug)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Debug)]
 pub struct Align32;
 
 /// Zero-sized type (ZST) to enforce 64-byte memory alignment
 #[repr(align(64))]
-#[derive(PartialEq, Eq, Copy, Clone, Debug)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Debug)]
 pub struct Align64;
 
 /// Zero-sized type (ZST) to enforce 128-byte memory alignment
 #[repr(align(128))]
-#[derive(PartialEq, Eq, Copy, Clone, Debug)]
+#[derive(PartialEq, Eq, PartialOrd, Ord, Copy, Clone, Debug)]
 pub struct Align128;
